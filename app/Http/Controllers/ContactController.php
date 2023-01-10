e12004f5a78f2ff56f385e29bd85a25c2111d644
kiril755 <kiril777kiril@gmail.com>
2023-01-10 22:19:41 +0200

    relocate app

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller {
-    public function submit(Request $req) {
-        dd($req->input('message'));
+    public function submit(ContactRequest $req) {
+        // $validation = $req->validate([
+        //     'subject' => 'required|min:5|max:50',
+        //     'message' => 'required|min:15|max:500'
+        // ]);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width= , initial-scale=1.0">
     <title>@yield('title-block')</title>
-    @vite(['resources/s css/app.scss', 'resources/js/app.js'])
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
