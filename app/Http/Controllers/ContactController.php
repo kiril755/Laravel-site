4aef3f6069049be274a43e14c3f1719d1274ee8d
kiril755 <kiril777kiril@gmail.com>
2023-01-11 19:52:35 +0200

    add delete fn in controller and add new routes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -36,4 +36,21 @@ class ContactController extends Controller {
 
         return view('update-message', ['data' => $contact->find($id)]);
     }
+
+    public function updateMessageSubmit($id, ContactRequest $req) {
+        $contact = Contact::find($id);
+        $contact->name = $req->input('name');
+        $contact->email = $req->input('email');
+        $contact->subject = $req->input('subject');
+        $contact->message = $req->input('message');
+
+        $contact->save();
+
+        return redirect()->route('contact-data-one', $id )->with('success', 'Сообщение было обновлено!');
+    }
+
+    public function deleteMessage($id) {
+        Contact::find($id)->delete();
+        return redirect()->route('contact-data')->with('success', 'Сообщение было удалено !');
+    }
 }
--- resources/views/update-message.blade.php
+++ resources/views/update-message.blade.php
@@ -5,29 +5,29 @@
 @section('content')
     <h1>Обновление записи</h1>
 
-    <form action="{{ route('contact-form') }}" method="post">
+    <form action="{{ route('contact-update-submit', $data->id) }}" method="post">
         @csrf 
 
         <div class="form-inline mb-3">
             <label for="name">Введите имя</label>
-            <input type="text" name="name" placeholder="введите имя" id="name" class="form-control">
+            <input type="text" name="name" value="{{ $data->name }}" placeholder="введите имя" id="name" class="form-control">
         </div>
 
         <div class="form-inline mb-3">
             <label for="email">Email</label>
-            <input type="text" name="email" placeholder="введите email" id="email" class="form-control">
+            <input type="text" name="email" value="{{ $data->email }}" placeholder="введите email" id="email" class="form-control">
         </div>
 
         <div class="form-inline mb-3">
             <label for="subject">Тема сообщения</label>
-            <input type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
+            <input type="text" name="subject" value="{{ $data->subject }}" placeholder="Тема сообщения" id="subject" class="form-control">
         </div>
 
         <div class="form-inline mb-3">
             <label for="message">Сообщение</label>
-            <textarea name="message" id="message" class="form-control" placeholder="введите сообщение"></textarea>
+            <textarea name="message" id="message"  class="form-control" placeholder="введите сообщение">{{ $data->message }}</textarea>
         </div>
 
-        <button type="submit" class="btn btn-success">Отправить</button>
+        <button type="submit" class="btn btn-success">Обновить</button>
     </form>
 @endsection 
